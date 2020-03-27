7dd34ab1440624934939db6d4b206cab5ea41033
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2020-03-27 17:38:51 -0300

    modify repository to extend from Repository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,8 @@
 package wolox.training.repositories;
+
+import org.springframework.data.repository.Repository;
 import wolox.training.models.Book;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends Repository<Book, Long> {
     Book findByAuthor(String author);
 }
-
