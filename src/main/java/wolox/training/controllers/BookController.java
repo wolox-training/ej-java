f7a0cca6e8dd9d1b026a45b676054db9c903068e
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2020-04-01 16:56:39 -0300

    handle exception for put method when mismatch id

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import wolox.training.exceptions.BookIdMismatchException;
 import wolox.training.exceptions.BookNotFoundException;
 import wolox.training.models.Book;
 import wolox.training.repositories.BookRepository;
@@ -52,6 +53,9 @@ public class BookController {
 
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
+        if (book.getId() != id) {
+            throw new BookIdMismatchException();
+        }
         bookRepository.findById(id)
             .orElseThrow(() -> new BookNotFoundException());
         return bookRepository.save(book);
