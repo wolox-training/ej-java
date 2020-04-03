878a2f327f5a16eaa2764f4e8464f58fe351f8ac
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2020-04-03 10:25:57 -0300

    add custom method to delete and add a book to user, add base path to book controller

--- src/main/java/wolox/training/controllers/UserController.java
+++ src/main/java/wolox/training/controllers/UserController.java
@@ -9,17 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import wolox.training.exceptions.BookNotFoundException;
 import wolox.training.exceptions.UserIdMismatchException;
 import wolox.training.exceptions.UserNotFoundException;
+import wolox.training.models.Book;
 import wolox.training.models.User;
+import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
 
+@RestController
+@RequestMapping("/api/users")
 @Controller
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
     @GetMapping
     public Iterable findAll() {
@@ -50,6 +59,31 @@ public class UserController {
         }
         userRepository.findById(id)
             .orElseThrow(() -> new UserNotFoundException());
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/{user_id}/books/{book_id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addBookToUser(@PathVariable Long user_id, @PathVariable Long book_id) {
+        User user = userRepository.findById(user_id)
+            .orElseThrow(() -> new UserNotFoundException());
+
+        Book book = bookRepository.findById(book_id)
+            .orElseThrow(() -> new BookNotFoundException());
+
+        user.addBook(book);
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{user_id}/books/{book_id}")
+    public User deleteBookToUser(@PathVariable Long user_id, @PathVariable Long book_id) {
+        User user = userRepository.findById(user_id)
+            .orElseThrow(() -> new UserNotFoundException());
+
+        Book book = bookRepository.findById(book_id)
+            .orElseThrow(() -> new BookNotFoundException());
+
+        user.deleteBook(book);
         return userRepository.save(user);
     }
 }
