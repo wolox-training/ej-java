ae6aa1eb7bc4c3781fa71b08051f401b59b57ed2
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2020-04-09 12:47:16 -0300

    add dependency to gradle, add tests for models

--- src/test/java/wolox/training/models/BookTest.java
+++ src/test/java/wolox/training/models/BookTest.java
@@ -3,11 +3,13 @@ package wolox.training.models;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import wolox.training.repositories.BookRepository;
 
@@ -20,20 +22,71 @@ public class BookTest {
     @Autowired
     private BookRepository bookRepository;
 
-    @Test
-    public void whenFindByName_thenReturnEmployee() {
-        // given
-        Book book = new Book("Mi isla", "Elisabet Benavent", "romance",
+    @MockBean
+    private Book book;
+
+    @Before
+    public void setUp() {
+        book = new Book("Mi isla", "Elisabet Benavent", "romance",
             "image.png", "De la autora de En los Zapatos de Valeria", "2018",
             "Suma de Letras", "9789877390957", 536);
+    }
+
+    @Test
+    public void whenFindByAuthor_thenReturnBook() {
         entityManager.persist(book);
         entityManager.flush();
 
-        // when
         Optional<Book> found = bookRepository.findByAuthor(book.getAuthor());
 
-        // then
-        assertThat(found.get().getAuthor())
-            .isEqualTo(book.getAuthor());
+        assertThat(found.get().getAuthor()).isEqualTo(book.getAuthor());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutTitle_thenThrowException() {
+        book.setTitle(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutAuthor_thenThrowException() {
+        book.setAuthor(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutImage_thenThrowException() {
+        book.setImage(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutSubtitle_thenThrowException() {
+        book.setSubtitle(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutPublisher_thenThrowException() {
+        book.setPublisher(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutYear_thenThrowException() {
+        book.setYear(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutIsbn_thenThrowException() {
+        book.setIsbn(null);
+        bookRepository.save(book);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenCreateBookWithoutPages_thenThrowException() {
+        book.setPages(null);
+        bookRepository.save(book);
     }
 }
