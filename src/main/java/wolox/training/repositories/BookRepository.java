851333cb6e111f477535a5cb2881f3a802804085
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2020-04-21 12:35:47 -0300

    set default values for controllers, add query for user repository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,7 +1,6 @@
 package wolox.training.repositories;
 import java.util.List;
 import java.util.Optional;
-import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +10,8 @@ public interface BookRepository extends CrudRepository<Book, Long> {
     Optional<Book> findByAuthor(String author);
     Optional<Book> findByIsbn(String isbn);
 
-    @Query("SELECT b FROM Book b WHERE (:publisher IS NULL OR b.publisher = :publisher) AND"
-        + " (:genre IS NULL OR b.genre = :genre) AND (:year IS NULL OR b.year = :year)")
+    @Query("SELECT b FROM Book b WHERE (:publisher IS '' OR b.publisher = :publisher) AND"
+        + " (:genre IS '' OR b.genre = :genre) AND (:year IS '' OR b.year = :year)")
     List<Book> findByPublisherAndGenreAndYear(
         @Param("publisher") String publisher,
         @Param("genre") String genre,
