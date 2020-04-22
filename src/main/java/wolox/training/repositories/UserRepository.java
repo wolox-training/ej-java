355004ef8875f3d2e050d4b1c60392dc05562583
Edkaryd Jimenez <edkaryd.jimenez@wolox.com.ar>
2020-04-22 14:53:45 -0300

    avoid extending from paging and sorting repository since de pagination info is getting duplicated

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -6,11 +6,11 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import wolox.training.models.User;
 
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUserName(String userName);
 
     @Query("SELECT u FROM User u WHERE (cast(:startDate as date) IS NULL OR u.birthDate >= :startDate) AND"
